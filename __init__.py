07bcc7065ad769b1dd30e9743dcad2ecde8c93fe
Chris Hemingway <chris.hemingway@gmail.com>
2022-06-04 14:35:48 +0100

    Fix help on display, move most elements into class

--- __init__.py
+++ __init__.py
@@ -9,68 +9,67 @@ MAX_BRIGHTNESS = 0.5
 
 TICK_MS = 25
 
-tidal.led_power_on(True)
-leds = NeoPixel(tidal.LED_DATA, NUM_LEDS)
-leds.fill((50, 50, 50))
-leds.write()
 
-rainbow_state = 0
-
-
-def rainbow():
-    global rainbow_state
-    for i in range(NUM_LEDS):
-        hue = ((i + rainbow_state) % NUM_LEDS) / NUM_LEDS
-        leds[i] = hsv_to_rgb(hue, 1, MAX_BRIGHTNESS)
-    leds.write()
-    rainbow_state += 3
-    if rainbow_state > NUM_LEDS:
-        rainbow_state = 0
-
-
-def all_on():
-    v = (255, 255, 255) * MAX_BRIGHTNESS
-    leds.fill(v)
-    leds.write()
-
-
-def all_off():
-    v = (0, 0, 0)
-    leds.fill(v)
-    leds.write()
-
-
-MODES = [
-    rainbow,
-    all_on,
-    all_off
-]
-
-current_mode = 0
-
-def toggle_mode():
-    global current_mode
-    current_mode = (current_mode + 1) % len(MODES)
-
-def update_leds():
-    MODES[current_mode]()
-
-class MyApp(TextApp):
+class RaveApp(TextApp):
 
     TITLE = "Neopixel Rave"
-    
+    BG = tidal.st7789.BLACK
+    FG = tidal.st7789.WHITE
+
+    def rainbow(self):
+        for i in range(NUM_LEDS):
+            hue = ((i + self.rainbow_state) % NUM_LEDS) / NUM_LEDS
+            self.leds[i] = hsv_to_rgb(hue, 1, MAX_BRIGHTNESS)
+        self.leds.write()
+        self.rainbow_state += 2
+        if self.rainbow_state > NUM_LEDS:
+            self.rainbow_state = 0
+
+    def all_on(self):
+        v = int(255 * MAX_BRIGHTNESS)
+        self.leds.fill((v, v, v))
+        self.leds.write()
+
+    def all_off(self):
+        v = (0, 0, 0)
+        self.leds.fill(v)
+        self.leds.write()
+
+    def setup_leds(self):
+        tidal.led_power_on(True)
+        self.leds = NeoPixel(tidal.LED_DATA, NUM_LEDS)
+        self.leds.fill((0, 0, 0))
+        self.leds.write()
+
+    def next_mode(self):
+        self.current_mode = (self.current_mode + 1) % len(self.MODES)
+
+    def update_leds(self):
+        self.MODES[self.current_mode]()
+
     def on_start(self):
         super().on_start()
-        self.buttons.on_press(tidal.JOY_UP, toggle_mode)
-        self.timer = self.periodic(TICK_MS, update_leds)
-        self.window.println("Press Joy Left to change mode!")
-        self.window.println("Press Joy up/down to change LEDs")
+        self.MODES = [self.rainbow, self.all_on, self.all_off]
+        self.rainbow_state = 0
+        self.current_mode = 0
+        self.buttons.on_press(tidal.JOY_LEFT, self.next_mode)
+        self.buttons.on_press(tidal.JOY_RIGHT, self.next_mode)
 
     def on_activate(self):
         super().on_activate()
+        self.setup_leds()
+        tidal.led_power_on(True)
+        # Timer needs to be here
+        self.timer = self.periodic(TICK_MS, self.update_leds)
+        # Screen writing needs to be here
+        self.window.cls()
+        self.window.println("L/R = Mode")
+        self.window.println("U/D = Brightness")
 
     def on_deactivate(self):
         super().on_deactivate()
+        tidal.led_power_on(False)
         self.timer.cancel()
 
-main = MyApp
+
+main = RaveApp
